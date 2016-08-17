9a19b089629fa62cf88160f0fd6d11e7a1e0e165
mayamcclatchy <mm5035@bard.edu>
2016-08-17 12:28:14 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -40,8 +40,9 @@ var rules = {
     "What do you think about computers?",
     "What do you think machines have to do with your problem?",
   ],
-  "~*x name ~*y": [
-    "I am not interested in names",
+  
+  "~*x what is my name? ~*y": [
+  	maya], 
   ],
   "~*x sorry ~*y": [
     "Please don't apologize",
